c54b0686500918f73861bafd508aee3238d7b2e3
june <lovekwon200@gmail.com>
2021-01-18 20:54:16 +0900

    2440

--- hundred/main.cpp
+++ hundred/main.cpp
@@ -1,17 +1,13 @@
 #include <iostream>
 using namespace std;
+
 int main() {
-    int arr[4];
-    int min;
-    //0:x, 1:y, 2:w, 3:h
-    cin >> arr[0] >> arr[1] >> arr[2] >> arr[3];
-    arr[2] = arr[2] - arr[0];
-    arr[3] = arr[3] - arr[1];
-    min = arr[0];
-    for (int i = 0; i < 4; i++) {
-        if (arr[i] < min)
-            min = arr[i];
+    int n;
+    cin >> n;
+    for (int i = n; i > 0; i--) {
+        for (int j = i; j > 0; j--)
+            cout << '*';
+        cout << endl;
     }
-    cout << min << endl;
     return 0;
 }
