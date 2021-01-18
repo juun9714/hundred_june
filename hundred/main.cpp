0c5938dc68aae4a634776c9759c712ced6b3c5cb
june <lovekwon200@gmail.com>
2021-01-18 20:42:19 +0900

    1085

--- hundred/main.cpp
+++ hundred/main.cpp
@@ -1,48 +1,17 @@
 #include <iostream>
-#include <cstdlib>
-
 using namespace std;
-
 int main() {
-    int n, sum;
-    sum = 0;
-    cin >> n;
-    char* str = new char[n + 1];
-    cin >> str;
-    for (int i = 0; i < n; i++) {
-        switch(str[i]){
-        case '0':
-            sum += 0;
-            break;
-        case '1':
-            sum += 1;
-            break;
-        case '2':
-            sum += 2;
-            break;
-        case '3':
-            sum += 3;
-            break;
-        case '4':
-            sum += 4;
-            break;
-        case '5':
-            sum += 5;
-            break;
-        case '6':
-            sum += 6;
-            break;
-        case '7':
-            sum += 7;
-            break;
-        case '8':
-            sum += 8;
-            break;
-        case '9':
-            sum += 9;
-            break;
-        }
+    int arr[4];
+    int min;
+    //0:x, 1:y, 2:w, 3:h
+    cin >> arr[0] >> arr[1] >> arr[2] >> arr[3];
+    arr[2] = arr[2] - arr[0];
+    arr[3] = arr[3] - arr[1];
+    min = arr[0];
+    for (int i = 0; i < 4; i++) {
+        if (arr[i] < min)
+            min = arr[i];
     }
-    cout << sum << endl;
+    cout << min << endl;
     return 0;
 }
