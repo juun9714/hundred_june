9cfd3f4b439bd5e219c0aa2d37b23424361d5d82
june <lovekwon200@gmail.com>
2021-01-18 21:07:46 +0900

    2441

--- hundred/main.cpp
+++ hundred/main.cpp
@@ -5,8 +5,12 @@ int main() {
     int n;
     cin >> n;
     for (int i = n; i > 0; i--) {
-        for (int j = i; j > 0; j--)
-            cout << '*';
+        for (int j = n;j>0;j--) {
+            if (j > i)
+                cout << ' ';
+            else
+                cout << '*';
+        }
         cout << endl;
     }
     return 0;
