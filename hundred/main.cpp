eebd0fba8bc48f6d56726f0f1c4854a186ecf21a
june <lovekwon200@gmail.com>
2021-01-20 18:51:30 +0900

    1267

--- hundred/main.cpp
+++ hundred/main.cpp
@@ -1,31 +1,33 @@
 #include <iostream>
 using namespace std;
+int young(int n, int* arr) {
+	int num = 0;
+	for (int i = 0; i < n; i++) {
+		num += (arr[i] / 30) + 1;
+	}
+	return 10 * num;
+}
+
+int min(int n, int* arr) {
+	int num = 0;
+	for (int i = 0; i < n; i++) {
+		num += (arr[i] / 60) + 1;
+	}
+	return 15 * num;
+}
 int main() {
-    int n,nn;
-    cin >> n;
-    for (int i = n; i > 0; i--) {
-        nn = n - i;
-        for (int ii = 0; ii < (2 * i - 1); ii++) {
-            if (nn > 0) {
-                for (; nn > 0; nn--) {
-                    cout << ' ';
-                }
-            }
-            cout << '*';
-        }
-        cout << endl;
-    }
-    for (int j = 2; j <= n; j++) {
-        nn = n - j;
-        for (int jj = 0; jj < (2 * j - 1); jj++) {
-            if (nn > 0) {
-                for (; nn > 0; nn--) {
-                    cout << ' ';
-                }
-            }
-            cout << '*';
-        }
-        cout << endl;
-    }
-    return 0;
+
+	int n;
+	cin >> n;
+	int* arr = new int[n];
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+	if (young(n, arr) < min(n, arr))
+		cout << "Y " << young(n, arr);
+	else if (young(n, arr) > min(n, arr))
+		cout << "M " << min(n, arr);
+	else
+		cout << "Y M " << young(n, arr);
+	return 0;
 }
