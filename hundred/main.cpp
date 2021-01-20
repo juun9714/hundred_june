aa6c821876dd3da7bb78d095ba7e203e2c47472c
june <lovekwon200@gmail.com>
2021-01-20 20:15:12 +0900

    1075

--- hundred/main.cpp
+++ hundred/main.cpp
@@ -1,33 +1,20 @@
 #include <iostream>
 using namespace std;
-int young(int n, int* arr) {
-	int num = 0;
-	for (int i = 0; i < n; i++) {
-		num += (arr[i] / 30) + 1;
-	}
-	return 10 * num;
-}
 
-int min(int n, int* arr) {
-	int num = 0;
-	for (int i = 0; i < n; i++) {
-		num += (arr[i] / 60) + 1;
-	}
-	return 15 * num;
-}
 int main() {
-
-	int n;
-	cin >> n;
-	int* arr = new int[n];
-	for (int i = 0; i < n; i++) {
-		cin >> arr[i];
+	int N, F, i,n;
+	cin >> N >> F;
+	N /= 100;
+	N *= 100;
+	n = N;
+	for (i = 0; i < 100; i++) {
+		N = n+i;
+		if (N % F == 0)
+			break;
 	}
-	if (young(n, arr) < min(n, arr))
-		cout << "Y " << young(n, arr);
-	else if (young(n, arr) > min(n, arr))
-		cout << "M " << min(n, arr);
+	if (i < 10)
+		cout << "0" << i << endl;
 	else
-		cout << "Y M " << young(n, arr);
+		cout << i << endl;
 	return 0;
 }
