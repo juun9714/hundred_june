d3705015363ea00f866dce4e3c8900790105e6d1
june <lovekwon200@gmail.com>
2021-01-21 15:59:53 +0900

    1152

--- hundred/main.cpp
+++ hundred/main.cpp
@@ -1,39 +1,54 @@
 #include <iostream>
 #include <cstring>
+#include <string>
 using namespace std;
 
+/*
+예외처리 해주어야 할 것
+1. 문자열 맨앞에만 공백
+2. 문자열 맨뒤에만 공백
+3. 문자열 맨앞 맨뒤 모두에 공백 1개씩
+4. 아무것도 입력되지 않았을 때 0 출력
+5. 공백 1개 딸랑 입력되었을 때 0 출력
+*/
+
+
 int main() {
-	char chess[8][8];
-	int check(0);
-	for (int i = 0; i < 8; i++) {
-		for (int j = 0; j < 8; j++)
-			cin >> chess[i][j];
-	}//체스판 위에 말 올려놓기
+	string str;
+	int idx = 0;
+	int num = 0;
+	getline(cin, str, '\n');
+	//std::cout << "idx is " << idx <<" and str length is "<<str.length()<< endl;
+	while (str[idx] == ' ')
+		idx++;
+	//문자열 초반의 공백 모두 처리 
+	//std::cout << "idx is " << idx << " and str length is " << str.length() << endl;
 
-	/*
-	cout << endl;
-	cout <<"체스판 출력 시작"<< endl;
 
-	for (int i = 0; i < 8; i++) {
-		for (int j = 0; j < 8; j++)
-			cout<< chess[i][j];
-		cout << endl;
-	}//체스판 출력
-	cout << "체스판 출력 끝" << endl;
-	*/
-	for (int i = 0; i < 8; i++) {
-		for (int j = 0; j < 8; j++) {
-			if (i % 2 == 0 && j % 2 == 0 && chess[i][j]=='F') {
-				//짝수행 && 짝수열
-				//cout << chess[i][j] << endl;
-				check++;
+	while (idx<=str.size()) {
+		if (str[idx] == ' ') {
+			if (idx+1==str.size()) {
+				//std::cout << "뒤에 공백임" << endl;
+				break;
 			}
-			else if (i % 2 == 1 && j % 2 == 1 && chess[i][j] == 'F') {
-				//cout << chess[i][j] << endl;
-				check++;
+			else {
+				num++;
+				//std::cout << "num increased " << num << " idx is " << idx << endl;
+				idx++;
 			}
 		}
-	}//흰 돌 위에 말 있는지 check
-	cout << check << endl;
+		else {
+			//std::cout << "num is not increased " << num << " idx is " << idx << endl;
+			idx++;
+		}
+	}
+	if (str.length() == 0)
+		cout << '0' << endl;
+	else if (str.length() == 1 && str[0] == ' ')
+		cout << '0' << endl;
+	else {
+		num++;
+		std::cout << num << std::endl;
+	}
 	return 0;
 }
