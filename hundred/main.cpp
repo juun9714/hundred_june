abce315448a8e3eaecabf2c20baf20197ff4562f
june <lovekwon200@gmail.com>
2021-01-21 15:01:38 +0900

    1100

--- hundred/main.cpp
+++ hundred/main.cpp
@@ -1,73 +1,39 @@
 #include <iostream>
 #include <cstring>
 using namespace std;
-struct valGop {
-	long long val;
-	long long gop;
-};
-valGop colorVal(char* color) {
-	valGop res;
-	if (!strncmp(color, "black", 5)) {
-		res.val = 0;
-		res.gop = 1;
-	}
-	else if (!strncmp(color, "brown", 5)) {
-		res.val = 1;
-		res.gop = 10;
-	}
-	else if (!strncmp(color, "red", 3)) {
-		res.val = 2;
-		res.gop = 100;
-	}
-	else if (!strncmp(color, "orange", 6)) {
-		res.val = 3;
-		res.gop = 1000;
-	}
-	else if (!strncmp(color, "yellow", 6)) {
-		res.val = 4;
-		res.gop = 10000;
-	}
-	else if (!strncmp(color, "green", 5)) {
-		res.val = 5;
-		res.gop = 100000;
-	}
-	else if (!strncmp(color, "blue", 4)) {
-		res.val = 6;
-		res.gop = 1000000;
-	}
-	else if (!strncmp(color, "violet", 6)) {
-		res.val = 7;
-		res.gop = 10000000;
-	}
-	else if (!strncmp(color, "grey", 4)) {
-		res.val = 8;
-		res.gop = 100000000;
-	}
-	else if(!strncmp(color, "white", 5)) {
-		res.val = 9;
-		res.gop = 1000000000;
-	}
-
-	return res;
-}
 
 int main() {
-	long long om=0;
-	valGop ret;
-	char color[7] = { '\0' };
-	cin >> color;
-	ret=colorVal(color);
-	om += 10 * ret.val;
+	char chess[8][8];
+	int check(0);
+	for (int i = 0; i < 8; i++) {
+		for (int j = 0; j < 8; j++)
+			cin >> chess[i][j];
+	}//체스판 위에 말 올려놓기
 
-	cin >> color;
-	ret = colorVal(color);
-	om += ret.val;
+	/*
+	cout << endl;
+	cout <<"체스판 출력 시작"<< endl;
 
-	cin >> color;
-	ret = colorVal(color);
-	om *= ret.gop;
-
-	cout << om << endl;
-	
+	for (int i = 0; i < 8; i++) {
+		for (int j = 0; j < 8; j++)
+			cout<< chess[i][j];
+		cout << endl;
+	}//체스판 출력
+	cout << "체스판 출력 끝" << endl;
+	*/
+	for (int i = 0; i < 8; i++) {
+		for (int j = 0; j < 8; j++) {
+			if (i % 2 == 0 && j % 2 == 0 && chess[i][j]=='F') {
+				//짝수행 && 짝수열
+				//cout << chess[i][j] << endl;
+				check++;
+			}
+			else if (i % 2 == 1 && j % 2 == 1 && chess[i][j] == 'F') {
+				//cout << chess[i][j] << endl;
+				check++;
+			}
+		}
+	}//흰 돌 위에 말 있는지 check
+	cout << check << endl;
 	return 0;
 }
