db2b9c1b44eaf934d0f4153226385c4d93d5eef9
june <lovekwon200@gmail.com>
2021-01-21 16:26:49 +0900

    1173

--- hundred/main.cpp
+++ hundred/main.cpp
@@ -3,52 +3,39 @@
 #include <string>
 using namespace std;
 
-/*
-예외처리 해주어야 할 것
-1. 문자열 맨앞에만 공백
-2. 문자열 맨뒤에만 공백
-3. 문자열 맨앞 맨뒤 모두에 공백 1개씩
-4. 아무것도 입력되지 않았을 때 0 출력
-5. 공백 1개 딸랑 입력되었을 때 0 출력
-*/
-
 
 int main() {
-	string str;
-	int idx = 0;
-	int num = 0;
-	getline(cin, str, '\n');
-	//std::cout << "idx is " << idx <<" and str length is "<<str.length()<< endl;
-	while (str[idx] == ' ')
-		idx++;
-	//문자열 초반의 공백 모두 처리 
-	//std::cout << "idx is " << idx << " and str length is " << str.length() << endl;
+	int N, m, M, T, R,low, total(0),work(0);
+	cin >> N >> m >> M >> T >> R;
+	low = m;
 
-
-	while (idx<=str.size()) {
-		if (str[idx] == ' ') {
-			if (idx+1==str.size()) {
-				//std::cout << "뒤에 공백임" << endl;
-				break;
-			}
-			else {
-				num++;
-				//std::cout << "num increased " << num << " idx is " << idx << endl;
-				idx++;
-			}
-		}
-		else {
-			//std::cout << "num is not increased " << num << " idx is " << idx << endl;
-			idx++;
-		}
-	}
-	if (str.length() == 0)
-		cout << '0' << endl;
-	else if (str.length() == 1 && str[0] == ' ')
-		cout << '0' << endl;
+	if (m + T > M)
+		cout << "-1" << endl;
 	else {
-		num++;
-		std::cout << num << std::endl;
+		while (1) {
+			if (work >= N)
+				break;
+			else {
+				if (m + T <= M) {
+					//현재 맥박<=최고 맥박 -> 운동
+					m += T;
+					work++;
+					total++;
+					//cout << "1분 운동 완료, 총 걸린 시간 현재 " << total << " 현재 맥박 " << m << endl;
+				}
+				else if (m + T > M) {
+					//현재 맥박>최고 맥박 -> 휴식 필요
+					if (m - R > low)
+						m -= R;
+					else
+						m = low;
+					total++;
+					//cout << "1분 휴식 완료, 총 걸린 시간 현재 " << total << " 현재 맥박 " << m << endl;
+				}
+			}
+		}
+		//cout << "work out time : " << work << " total time : " << total << endl;
+		cout << total << endl;
 	}
 	return 0;
 }
