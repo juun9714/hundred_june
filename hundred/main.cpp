61884dc47e60fd03f45ba567af89cd1e1facd33b
june <lovekwon200@gmail.com>
2021-01-22 22:52:12 +0900

    1236

--- hundred/main.cpp
+++ hundred/main.cpp
@@ -1,287 +1,137 @@
 #include <iostream>
 #include <cstdlib>
-#include <string>
+#include <cstring>
 using namespace std;
+/*
+행기준, 열기준으로 둘다 해야 !!!
+*/
 
-int alphaToNum(char alpha) {
-	int ret(0);
-	switch (alpha) {
-	case 'a':
-		ret = 0;
-		break;
-	case 'A':
-		ret = 0;
-		break;
-	case 'b':
-		ret = 1;
-		break;
-	case 'B':
-		ret = 1;
-		break;
-	case 'c':
-		ret = 2;
-		break;
-	case 'C':
-		ret = 2;
-		break;
-	case 'd':
-		ret = 3;
-		break;
-	case 'D':
-		ret = 3;
-		break;
-	case 'e':
-		ret = 4;
-		break;
-	case 'E':
-		ret = 4;
-		break;
-	case 'f':
-		ret = 5;
-		break;
-	case 'F':
-		ret = 5;
-		break;
-	case 'g':
-		ret = 6;
-		break;
-	case 'G':
-		ret = 6;
-		break;
-	case 'h':
-		ret = 7;
-		break;
-	case 'H':
-		ret = 7;
-		break;
-	case 'i':
-		ret = 8;
-		break;
-	case 'I':
-		ret = 8;
-		break;
-	case 'j':
-		ret = 9;
-		break;
-	case 'J':
-		ret = 9;
-		break;
-	case 'k':
-		ret = 10;
-		break;
-	case 'K':
-		ret = 10;
-		break;
-	case 'l':
-		ret = 11;
-		break;
-	case 'L':
-		ret = 11;
-		break;
-	case 'm':
-		ret = 12;
-		break;
-	case 'M':
-		ret = 12;
-		break;
-	case 'n':
-		ret = 13;
-		break;
-	case 'N':
-		ret = 13;
-		break;
-	case 'o':
-		ret = 14;
-		break;
-	case 'O':
-		ret = 14;
-		break;
-	case 'p':
-		ret = 15;
-		break;
-	case 'P':
-		ret = 15;
-		break;
-	case 'q':
-		ret = 16;
-		break;
-	case 'Q':
-		ret = 16;
-		break;
-	case 'r':
-		ret = 17;
-		break;
-	case 'R':
-		ret = 17;
-		break;
-	case 's':
-		ret = 18;
-		break;
-	case 'S':
-		ret = 18;
-		break;
-	case 't':
-		ret = 19;
-		break;
-	case 'T':
-		ret = 19;
-		break;
-	case 'u':
-		ret = 20;
-		break;
-	case 'U':
-		ret = 20;
-		break;
-	case 'v':
-		ret = 21;
-		break;
-	case 'V':
-		ret = 21;
-		break;
-	case 'w':
-		ret = 22;
-		break;
-	case 'W':
-		ret = 22;
-		break;
-	case 'x':
-		ret = 23;
-		break;
-	case 'X':
-		ret = 23;
-		break;
-	case 'y':
-		ret = 24;
-		break;
-	case 'Y':
-		ret = 24;
-		break;
-	case 'z':
-		ret = 25;
-		break;
-	case 'Z':
-		ret = 25;
-		break;
-	}
-	return ret;
-}
 
-char numToAlpha(int num) {
-	char ret='A';
-	switch (num) {
-	case 0:
-		ret = 'A';
-		break;
-	case 1:
-		ret = 'B';
-		break;
-	case 2:
-		ret = 'C';
-		break;
-	case 3:
-		ret = 'D';
-		break;
-	case 4:
-		ret = 'E';
-		break;
-	case 5:
-		ret = 'F';
-		break;
-	case 6:
-		ret = 'G';
-		break;
-	case 7:
-		ret = 'H';
-		break;
-	case 8:
-		ret = 'I';
-		break;
-	case 9:
-		ret = 'J';
-		break;
-	case 10:
-		ret = 'K';
-		break;
-	case 11:
-		ret = 'L';
-		break;
-	case 12:
-		ret = 'M';
-		break;
-	case 13:
-		ret = 'N';
-		break;
-	case 14:
-		ret = 'O';
-		break;
-	case 15:
-		ret = 'P';
-		break;
-	case 16:
-		ret = 'Q';
-		break;
-	case 17:
-		ret = 'R';
-		break;
-	case 18:
-		ret = 'S';
-		break;
-	case 19:
-		ret = 'T';
-		break;
-	case 20:
-		ret = 'U';
-		break;
-	case 21:
-		ret = 'V';
-		break;
-	case 22:
-		ret = 'W';
-		break;
-	case 23:
-		ret = 'X';
-		break;
-	case 24:
-		ret = 'Y';
-		break;
-	case 25:
-		ret = 'Z';
-		break;
-	}
-	return ret;
-}
-
-struct MAX {
-	int num;
-	char alpha;
-	int check;
-};
 int main() {
-	int arr[26] = { 0 };
-	MAX max;
-	string str;
-	cin >> str;
-	for (int i = 0; i < str.size(); i++) {
-		arr[alphaToNum(str[i])]++;
+	int a(0), b(0),i(0),j(0),check_r(0),check_c(0),last(0);
+	cin >> a >> b;
+	char** castle = new char* [a];
+	int* check_bx = new int[b];
+	int* check_ax = new int[b];
+	for (i = 0; i < a; i++) {
+		castle[i] = new char[b];
+		memset(castle[i], '.', sizeof(a));
 	}
-	max.num = 0;
-	max.alpha = 'A';
-	max.check = 0;
-	for (int i = 0; i < 26; i++) {
-		if (max.num < arr[i]) {
-			max.alpha = numToAlpha(i);
-			max.num = arr[i];
-			max.check = 0;
-		}
-		else if (max.num == arr[i]) {
-			max.check = 1;
-		}
-	}
-	
-	if (max.check == 1)
-		cout << '?' << endl;
-	else if (max.check == 0)
-		cout << max.alpha << endl;
+	//2차원 배열 동적할당
 
+
+	for (i = 0; i < a; i++) {
+		for (j = 0; j < b; j++) {
+			cin >> castle[i][j];
+		}
+	}
+	//현재 성 관리원 상태 입력
+	
+
+	//행 기준
+	for (i = 0; i < a; i++) {
+		check_r = 0;
+		check_c = 0;
+		//check 초기화
+		for (j = 0; j < b; j++) {
+			if (castle[i][j] == 'X') {
+				check_r= 1;
+				break;
+				//해당 행에 관리원 있으니 더 볼 필요 없음(안쪽 for문 탈출) 
+			}
+		}
+
+		if (check_r == 1) //i 행에 관리원 있음 -> 다음행 체크
+			continue;
+		else if (check_r == 0) { //i 행에 관리원 없음 -> i행 중 관리원 없는 열 찾아야 함
+			memset(check_bx, 0, sizeof(int) * b);
+			for (int k = 0; k < b; k++) {
+				//열 기준으로 경비원이 없는 열을 찾아라 (현재 i 행 기준)
+				for (int l = 0; l < a; l++) {
+					if (castle[l][k] == 'X'){
+						check_bx[k] = 1;
+						break;
+					}
+				}
+			}
+			int done(0);
+			for (int g = 0; g < b; g++) {
+				if (check_bx[g] == 1)
+					continue;
+				else if (check_bx[g] == 0) {
+					castle[i][g] = 'X';
+					last++;
+					done = 1;
+					break;
+				}
+			}
+			if (done == 0) {
+				castle[i][0] = 'X';
+				last++;
+				//각 열에 모두 경비원 있지만 i행에는 없을 때 -> 그냥 i행 0열에 경비원 둬버려 
+			}
+
+		}
+	}
+
+	//열기준
+
+	for (j = 0; j < b; j++) {
+		check_c = 0;
+		//check 초기화
+		for (i = 0; i < a; i++) {
+			if (castle[i][j] == 'X') {
+				check_c = 1;
+				break;
+				//해당 열에 관리원 있으니 더 볼 필요 없음(안쪽 for문 탈출) 
+			}
+		}
+
+		if (check_c == 1) //j열에 관리원 있음 -> 다음열 체크
+			continue;
+		else if (check_c == 0) { //j열에 관리원 없음 -> j열 중 관리원 없는 행 찾아야 함
+			memset(check_ax, 0, sizeof(int) * a);
+			for (int l = 0; l < a; l++) {
+				//행 기준으로 경비원이 없는 행을 찾아라 (현재 j열 기준)
+				for (int k = 0; k < b; k++) {
+					if (castle[l][k] == 'X') {
+						check_ax[l] = 1;
+						break;
+					}
+				}
+			}
+			int done(0);
+			for (int g = 0; g < a; g++) {
+				if (check_ax[g] == 1)
+					continue;
+				else if (check_ax[g] == 0) {
+					castle[g][j] = 'X';
+					done = 1;
+					last++;
+					break;
+				}
+			}
+			if (done == 0) {
+				castle[0][j] = 'X';
+				last++;
+			}
+
+		}
+	}
+	/*
+	cout << endl;
+	for (i = 0; i < a; i++) {
+		for (j = 0; j < b; j++) {
+			cout<< castle[i][j];
+		}
+		cout << endl;
+	}
+	*/
+	cout << last << endl;
+
+
+	delete[] castle;
+	delete[] check_bx;
+	delete[] check_ax;
 	return 0;
 }
