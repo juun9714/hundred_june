71b6ca16f728c977646bbb6701f8f943e84e2b00
june <lovekwon200@gmail.com>
2021-02-10 17:26:54 +0900

    1356

--- hundred/main.cpp
+++ hundred/main.cpp
@@ -1,26 +1,78 @@
 #include <iostream>
 using namespace std;
 /*
-한번에 맞았당 !
+한번에 맞음
 */
-int main() {
-	int A, B, N;
-	cin >> A >> B >> N;
-	int Q = 0, R = 0;
-	Q = A / B;
-	R = A % B;
+int atoi(string str, int i) {
+	switch (str[i]) {
+	case '0':
+		return 0;
+	case '1':
+		return 1;
+	case '2':
+		return 2;
+	case '3':
+		return 3;
+	case '4':
+		return 4;
+	case '5':
+		return 5;
+	case '6':
+		return 6;
+	case '7':
+		return 7;
+	case '8':
+		return 8;
+	case '9':
+		return 9;
+	}
+}
 
-	for (int i = 0; i < N; i++) {
-		R *= 10;
-		Q = R / B;
-		R = R % B;
-		if (R == 0 && i + 1 < N) {
-			Q = 0;
-			//cout << "Q is zero : " << Q << endl;
+int main() {
+	string str;
+	cin >> str;
+	int len = str.size();
+	int front = 1, back = 1, check = 0;
+	if (len == 1) {
+		//입력이 한자리 수일 때 항상 NO
+		cout << "NO";
+		return 0;
+	}
+	
+	//입력이 한자리 수가 아닐 때 
+	for (int i = 0; i < len - 1; i++) {
+		back = 1;
+		front = 1;
+		for (int j = 0; j <= i; j++) {
+			if (i == 0) {
+				front = atoi(str, 0);
+				//cout << "front digit is only one and front is " << front << endl;
+				break;
+			}
+			front *= atoi(str, j);
+			//cout << "now i and j is " << i <<" "<<j<< " and front is " << front << endl;
+		}
+
+		for (int k = i+1; k < len; k++) {
+			if (i + 1 == len - 1) {
+				back = atoi(str, len - 1);
+				//cout << "back digit is only one and front is " << back << endl;
+				break;
+			}
+			back *= atoi(str, k);
+			//cout << "now i and k is " << i << " " << k << " and back is " << back << endl;
+		}
+
+		//cout << endl;
+
+		if (front == back) {
+			check = 1;
 			break;
 		}
-		//cout << "Try "<<i<<": Q is " << Q << ", and R is " << R << endl;
 	}
-	cout << Q;
+	if (check == 1)
+		cout << "YES";
+	else if (check == 0)
+		cout << "NO";
 	return 0;
 }
