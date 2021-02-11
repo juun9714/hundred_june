85274494dbdf5ceb33b817596bf4c49e9cd05968
june <lovekwon200@gmail.com>
2021-02-11 15:51:02 +0900

    2609

--- hundred/main.cpp
+++ hundred/main.cpp
@@ -1,59 +1,39 @@
 #include <iostream>
 using namespace std;
-
-/*
-int atoi(string str, int i) {
-	switch (str[i]) {
-	case '0':
-		return 0;
-	case '1':
-		return 1;
-	case '2':
-		return 2;
-	case '3':
-		return 3;
-	case '4':
-		return 4;
-	case '5':
-		return 5;
-	case '6':
-		return 6;
-	case '7':
-		return 7;
-	case '8':
-		return 8;
-	case '9':
-		return 9;
-	}
-}
-*/
-
-
-/*
-https://m.blog.naver.com/PostView.nhn?blogId=occidere&logNo=220876138317&proxyReferer=https:%2F%2Fwww.google.com%2F
-참고 했음 .. 어렵다 DP문제 
-*/
-
+//GCD 최대공약수, LCM 최소공배수 
 int main() {
-	int a,n,m;
-	cin>> a;
-	int i = 0, j = 0, k = 0;
+	int large, small;
+	cin >> large >> small;
 
-	for (int aa = 0; aa < a; aa++) {
-		cin >> n >> m;
-		int bridge[30][30] = { 0 };
-		for (i = 1; i <= 30; i++)
-			bridge[1][i] = i;
-		//기초재료
-
-		for (i = 2; i <= n; i++) {
-			for (j = i; j <= m; j++) {
-				for (k = j; k >= i; k--)
-					bridge[i][j] += bridge[i - 1][k - 1];
-			}
-		}
-
-		cout << bridge[n][m] << endl;
+	if (large < small) {
+		int tmp = large;
+		large = small;
+		small = tmp;
 	}
+	//큰수 작은수 분리 
+	int gcd = small;
+	//최대 공약수 
+	while (1) {
+		if (gcd <=1) {
+			//1이 아닌 최대공약수가 없는 경우
+			gcd = 1;
+			break;
+		}else if (large % gcd == 0 && small % gcd == 0) //최대공약수 발견
+			break;
+		else
+			gcd--;
+			//small에서부터 줄여나가야 가장 큰 공약수가 채택된다. 
+	}
+
+	int lcm = 1;
+	while (1) {
+		//lcm이 small이거나 (최소공배수가 두 수의 곱이거나), large 또는 large*자연수(<small)가 최소공배수이거나 
+		if (lcm == small || (large * lcm) % small == 0)
+			break;
+		else
+			lcm++;
+	}
+	cout << gcd << endl;
+	cout<< lcm * large << endl;
 	return 0;
 }
