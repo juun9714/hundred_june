4f110c0281f3d614d9f90bd0a882bea30753e481
june <lovekwon200@gmail.com>
2021-02-11 16:08:54 +0900

    2693 버블정렬

--- hundred/main.cpp
+++ hundred/main.cpp
@@ -1,39 +1,25 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
-//GCD 최대공약수, LCM 최소공배수 
 int main() {
-	int large, small;
-	cin >> large >> small;
+	int arr[10] = { 0 };
+	int n,tmp=0;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < 10; j++)
+			cin >> arr[j];
 
-	if (large < small) {
-		int tmp = large;
-		large = small;
-		small = tmp;
+		for (int p = 0; p < 10; p++) {
+			for (int q = 0; q < 9; q++) {
+				if (arr[q] > arr[q + 1]){
+					tmp = arr[q];
+					arr[q] = arr[q + 1];
+					arr[q + 1] = tmp;
+				}
+			}
+		}
+		cout << arr[7] << endl;
 	}
-	//큰수 작은수 분리 
-	int gcd = small;
-	//최대 공약수 
-	while (1) {
-		if (gcd <=1) {
-			//1이 아닌 최대공약수가 없는 경우
-			gcd = 1;
-			break;
-		}else if (large % gcd == 0 && small % gcd == 0) //최대공약수 발견
-			break;
-		else
-			gcd--;
-			//small에서부터 줄여나가야 가장 큰 공약수가 채택된다. 
-	}
-
-	int lcm = 1;
-	while (1) {
-		//lcm이 small이거나 (최소공배수가 두 수의 곱이거나), large 또는 large*자연수(<small)가 최소공배수이거나 
-		if (lcm == small || (large * lcm) % small == 0)
-			break;
-		else
-			lcm++;
-	}
-	cout << gcd << endl;
-	cout<< lcm * large << endl;
+	
 	return 0;
 }
